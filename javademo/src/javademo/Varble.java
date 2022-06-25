15675ce4c3438b81f6252941d927829e00268276
akula <akula@SUMA.bbrouter>
2022-06-25 12:03:57 +0530

    variable

--- javademo/src/javademo/Varble.java
+++ javademo/src/javademo/Varble.java
@@ -1,10 +1,1 @@
-package javademo;
-
-public class Varble {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("suma");
-	}
-
-}
+package 
---
+++ javademo/src/javademo/integer.java
@@ -0,0 +1,15 @@
+package javademo;
+
+public class integer {
+
+	public static void main(String[] args)
+	{
+	int max=21474;
+	int min=-21476;
+	short shortmax=327;
+	short shortmin=327;
+	long longmax=97345252626L;
+	long longmin=97345252627L;
+	System.out.println("int"+max);
+		}
+}
